d4d939c89341ec9175c385e586aa66a4dd115e98
kuromadara <dowarah.dayananda35@gmail.com>
2022-08-21 01:00:31 +0530

    responsive navbar

--- Components/Navigation.tsx
+++ Components/Navigation.tsx
@@ -1,33 +1,83 @@
 
 import Link from "next/link";
+import { useState } from 'react';
 
 const Navigation = () => {
+    const [navbar, setNavbar] = useState(false);
     return (
+        
         <nav className="bg-body border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
             &#128512;
-            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <li >
-                        <Link href="/">
-                            <a>&#127968;</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/about-us">
-                            <a>&#129412;</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/contact-us">
-                            <a>&#128222;</a>
-                        </Link>
-                    </li>
-                </ul>
             </div>
+              <div className="md:hidden">
+                <button
+                  className="p-2 w-[7vh] h-[7vh] text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 text-white"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 text-white"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
             </div>
-            
-        </nav>
+          </div>
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                navbar ? 'block' : 'hidden'
+              }`}
+            >
+              <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li >
+                            <Link href="/">
+                                <a>&#127968;</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/about-us">
+                                <a>&#129412;</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/contact-us">
+                                <a>&#128222;</a>
+                            </Link>
+                        </li>
+                    </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
     );
 }
 

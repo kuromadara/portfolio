d801ae411c8f9caaf4e18371de644a38d83c2d1e
kuromadara <dowarah.dayananda35@gmail.com>
2022-08-21 14:48:09 +0530

    almost done

--- Components/ProjectItem/ProjectItem.tsx
+++ Components/ProjectItem/ProjectItem.tsx
@@ -4,7 +4,7 @@ function ProjectItem(props: any) {
     const { title, image, description, detail} = props;
     return(
         <div className="container mx-auto px-4 py-8 flex items-center justify-center">
-          <div className="box-border flex flex-col items-center w-auto h-auto lg:h-[55vh] shadow-light-card rounded-card mx-auto p-4 border-2 rounded">
+          <div className="box-border flex flex-col items-center w-auto h-auto lg:h-[62vh] shadow-light-card rounded-card mx-auto p-4 border-2 rounded">
             <img
                 src={image}
                 className="rounded-image m-x-auto h-[20vh] w-[20vh] mt-5 shadow-light-button"
@@ -12,11 +12,13 @@ function ProjectItem(props: any) {
             />
             <div className="text-left font-pixel">
                 <h1 className="font-extrabold text-2xl text-center">{title}</h1>
+                <br/>
                 <strong>
                     <div className="font-about text-left text-sm">{description}</div>
                 </strong>
+                
                 <em>
-                    <div className="font-about text-left text-sm">{detail}</div>
+                    <div className="font-about text-left text-sm mt-2">{detail}</div>
                 </em>
                     
             </div>
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -6,38 +6,32 @@ function ProjectsPage() {
 const PROJECTS = [
     {
         id: 1,
-        title: 'Cervical Cancer Screening',
-        image: 'https://raw.githubusercontent.com/kuromadara/filehost/main/media/icons/cell.svg',
-        description: "A Cervical cancer Screening app using Pap-Smear Slides",
-        detail: "Hello there, This was my Thesis for MCA at Cotton University. The model for prediction was trained using Keras on top of tensorflow and the web app was built uising Django",
+        title: 'Handwriting Recognition App',
+        image: '/static/images/pngegg.png',
+        description: "An Android app that can recognize on-screen handwriting and translate it to text.",
+        detail: "Hello there, This was my 5th Semester project for MCA at Cotton University. The app uses android Canvas API to draw on the screen and then uses android Digital Ink Recognition model to recognize the drawn handwriting and translate it to text. The app is built using Java and Android Studio.",
     },
     {
         id: 2,
-        title: 'Tick-Tak-Toe',
-        image: '/static/images/tic-tac-toe.png',
+        title: 'Cervical Cancer Screening Web App',
+        image: 'https://raw.githubusercontent.com/kuromadara/filehost/main/media/icons/cell.svg',
         description: "A Cervical cancer Screening app using Pap-Smear Slides",
         detail: "Hello there, This was my Thesis for MCA at Cotton University. The model for prediction was trained using Keras on top of tensorflow and the web app was built uising Django",
     },
     {
         id: 3,
         title: 'Tick-Tak-Toe',
-        image: '/static/images/pngegg.png',
-        description: "A Cervical cancer Screening app using Pap-Smear Slides",
-        detail: "Hello there, This was my Thesis for MCA at Cotton University. The model for prediction was trained using Keras on top of tensorflow and the web app was built uising Django",
+        image: '/static/images/tic-tac-toe1.png',
+        description: "A 2-Player CLI game of Tic-Tac-Toe",
+        detail: "Hello there, This is just a simple game of Tic-Tac-Toe. The game can be played using the terminal or the command line. This was built using Python and just one external library to color the output in the terminal. The interesting part is unlike other tick-tak-toe games, the game is can be played on a board of NxN size.",
     },
-    {
-        id: 4,
-        title: 'Tick-Tak-Toe',
-        image: 'https://randomuser.me/api/portraits/women/81.jpg',
-        description: "A Cervical cancer Screening app using Pap-Smear Slides",
-        detail: "Hello there, This was my Thesis for MCA at Cotton University. The model for prediction was trained using Keras on top of tensorflow and the web app was built uising Django",
-    }
+    
 ]
   return (
     <div className="bg-body h-auto">
         <div className="container mx-auto px-4 py-8 ">
           <h1 className="text-7xl font-bold text-center font-pixel">Projects</h1>
-        <div className="grid place-content-center grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-4">
+        <div className="grid place-content-center grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-3">
           {     
                 PROJECTS.map(project => (
                     <div className="" key={project.id}>
